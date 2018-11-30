0bd7bba36f0c40d9386012f5a7d06bca08851c73
Strigalet <strigalet999@gmail.com>
2018-11-30 19:14:26 +0300

    Cursed work

--- Curse_work/Cursed_Newton.py
+++ Curse_work/Cursed_Newton.py
@@ -5,17 +5,20 @@ def coefs():
     global n
     cfs = []
     try:
-        n = int(input("Введіть порядок вашого поліному: "))
+        n = int(input("Enter the order of your polynomial: "))
     except ValueError:
-        print("Введіть коректні дані")
+        print("Enter the correct data")
+        return coefs()
+    if n < 0 or n >50:
+        print("Enter the correct data")
         return coefs()
     i = n
     while len(cfs) <= n:
-        print("Введіть коефіцієнт до відповідного елементу зі степенню: ", i)
+        print("Enter the coefficient to the corresponding item with the power of: ", i)
         j = input()
         while not re.match(pattern_float, j) and not re.match(pattern_int, j):
-            print("Вводьте тiльки дiйснi числа, будь ласка")
-            print("Введіть коефіцієнт до відповідного елементу зі степенню: ", i)
+            print("Enter valid numbers only, please")
+            print("Enter the coefficient to the corresponding item with the power of: ", i)
             j = input()
         cfs.append(float(j))
         i = i - 1
@@ -55,49 +58,11 @@ def f(x, g, w):
     if g > n:
         return 0
 
-
-pattern_int = r"^[-\d]\d*$"
-pattern_float = r"^[-\d]\d*\.\d*$"
-g = 0
-w = coefs()
-a = set()
-less_than_abs_one=[-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
-for x0 in range(-1000000, 1000000, 100000):
-    c = newtons_method(f, df, x0, 1e-5)
-    a.add(c)
-for x0 in range(-100000, 100000, 10000):
-    c = newtons_method(f, df, x0, 1e-5)
-    a.add(c)
-for x0 in range(-10000, 10000, 1000):
-    c = newtons_method(f, df, x0, 1e-5)
-    a.add(c)
-for x0 in range(-1000, 1000, 100):
-    c = newtons_method(f, df, x0, 1e-5)
-    a.add(c)
-for x0 in range(-100, 100, 10):
-    c = newtons_method(f, df, x0, 1e-5)
-    a.add(c)
-for x0 in range(-10, 10, 1):
-    c = newtons_method(f, df, x0, 1e-5)
-    a.add(c)
-for x0 in less_than_abs_one:
-    c = newtons_method(f, df, x0, 1e-5)
-    a.add(c)
-if (len(a) == 1 and None in a) or (len(a) == 0):
-    print("No roots")
-else:
-    if len(a) < 100:
-        for i in a:
-            if i != None:
-                print("x=", i)
-    else:
-        print("Коренiв нескiнченна кiлькiсть")
-
-answer = input("Type 'stop' if u want to stop the prog: ")
-while answer != 'stop':
+def real():
+    global g,w
     g = 0
-    a = set()
     w = coefs()
+    a = set()
     for x0 in range(-1000000, 1000000, 100000):
         c = newtons_method(f, df, x0, 1e-5)
         a.add(c)
@@ -127,5 +92,15 @@ while answer != 'stop':
                 if i != None:
                     print("x=", i)
         else:
-            print("Коренiв нескiнченна кiлькiсть")
+            print("Infinite number of roots")
+
+
+
+pattern_int = r"^[-\d]\d*$"
+pattern_float = r"^[-\d]\d*\.\d*$"
+less_than_abs_one=[-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
+real()
+answer = input("Type 'stop' if u want to stop the prog: ")
+while answer != 'stop':
+    real()
     answer = input("Type 'stop' if u want to stop the prog: ")
